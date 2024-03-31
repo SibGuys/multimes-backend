3d53aa9d6af7df13f282b4023c5949cb212c3243
Ruslan Karabalin <karabalin112@gmail.com>
2024-03-31 20:40:52 +0600

    fix: cors for all vite

--- src/main/java/org/multimes/backend/core/web/controller/MessagesController.java
+++ src/main/java/org/multimes/backend/core/web/controller/MessagesController.java
@@ -5,14 +5,12 @@ import java.util.List;
 import org.multimes.backend.core.tg.handler.Updater;
 import org.multimes.backend.core.web.model.Message;
 import org.multimes.backend.core.web.service.interfaces.IMessageService;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin
 @RestController
 @RequestMapping("/messages")
 public class MessagesController {
